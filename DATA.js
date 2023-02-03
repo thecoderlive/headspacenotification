70ccf7d339cbbb49adf6e7debd0eb303403058bf
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-03 15:35:21 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,48 @@
+export const item = {
+    "id": "76b15ce2-2bf4-4572-80c2-01950b19eed9",
+    "background_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png",
+    "time_title": "Just health mention wear realize across. Prepare election art safe.",
+    "about_time": "Heavy community model federal. Maybe score appear stand state draw. Score late this difference film yourself.",
+    "enable_feature_list": [
+        {
+            "id": "0e3a1bcd-a10f-4dee-848e-6eca20869a1a",
+            "feature_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png",
+            "feature_title": "Price yes religious. As different class collection because worker.",
+            "about_feature": "Drug probably see event me important hand.",
+            "cross_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png",
+            "enable_feature": "Player true visit management maintain. Step job provide."
+        },
+        {
+            "id": "870ccc9e-7205-4017-8f74-144a899e07b3",
+            "feature_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/62.png",
+            "feature_title": "Of support citizen star. Into word wish teach specific magazine end. Little film until us guy political.",
+            "about_feature": "Drug probably see event me important hand.",
+            "cross_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/351.png",
+            "enable_feature": "Recognize continue allow story bill free drop. Mouth action thought mission dinner capital ball. Watch nearly building stage collection contain difficult."
+        },
+        {
+            "id": "7097b004-c082-4c31-8250-361ac851f1d4",
+            "feature_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/54.png",
+            "feature_title": "Have including Republican night according eat. Agency attack evidence single. Focus indicate condition.",
+            "about_feature": "Wonder forward major piece. Current across protect before we.",
+            "cross_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png",
+            "enable_feature": "History also condition system. Sound born help pull."
+        },
+        {
+            "id": "a16c4ca6-cef1-427a-b8d6-fe169a48c95d",
+            "feature_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png",
+            "feature_title": "Down up crime perhaps remain interest national tonight. Final record common hand. Deal call knowledge now president lawyer.",
+            "about_feature": "Name will bring. Shake small western.",
+            "cross_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png",
+            "enable_feature": "Leg economy southern manager statement fund. Most able learn mouth. Prove listen relationship meeting."
+        },
+        {
+            "id": "3ecb8574-67c6-401e-8194-41212c1a5c05",
+            "feature_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/53.png",
+            "feature_title": "Our model magazine type. Night pressure some by capital article big.",
+            "about_feature": "Test take really one manager body specific include. Far catch police like new.",
+            "cross_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/78.png",
+            "enable_feature": "Actually after career lose author system across. News already what part image society left always. Live region factor available every move."
+        }
+    ]
+}
